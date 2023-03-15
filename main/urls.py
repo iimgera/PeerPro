e8a322fc4dd59c10c5c543cbc7d6c1c6828b43d9
iimgera <iimgera28062004@gmail.com>
2023-03-16 04:40:55 +0600

    merge done

--- main/urls.py
+++ main/urls.py
@@ -12,43 +12,9 @@ from apps.users.views import (
     UserListView, 
     UserRetrieveUpdateDestroyView
     )
-
-
-
-
-
-
-schema_view = get_schema_view(
-   openapi.Info(
-      title="API",
-      default_version='v1',
-      description="Test description",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
-
-
-api_v1 = [
-    path('register/', views.RegistrationView.as_view()),
-    path('auth/', views.LoginView.as_view()),
-
-    path('users/', UserListView.as_view(), name='user-list'),
-    path('users/create/', UserCreateView.as_view(), name='user-create'),
-    path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
-
-]
-from django.urls import path, include 
-
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
 from apps.dashboard.views import (
-    ReportList, ReportDetail, 
+    ReportList, 
+    ReportDetail, 
     ReportExcelView, 
     )
 
@@ -66,8 +32,15 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+api_v1 = [
+    path('register/', views.RegistrationView.as_view()),
+    path('auth/', views.LoginView.as_view()),
 
+    path('users/', UserListView.as_view(), name='user-list'),
+    path('users/create/', UserCreateView.as_view(), name='user-create'),
+    path('users/<int:pk>/', UserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
 
+]
 
 
 urlpatterns = [
@@ -75,9 +48,11 @@ urlpatterns = [
     path('api/v1/', include(api_v1)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
+
     path('reports/', ReportList.as_view(), name='report-list'),
     path('reports/<int:pk>/', ReportDetail.as_view(), name='report-detail'),
     path('report/export/', ReportExcelView.as_view(), name='report-export'),
+
     path('teams/',include('apps.dashboard.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
